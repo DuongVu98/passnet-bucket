52fadaf4490eb2343cf229d19517ce9a7cc98e80
DuongVu98 <duongvutung1998@gmail.com>
2021-08-13 03:24:51 +0700

    format code

--- src/main/java/com/iucse/passnetbucket/domain/exception/FileNameExistedException.java
+++ src/main/java/com/iucse/passnetbucket/domain/exception/FileNameExistedException.java
@@ -1,6 +1,6 @@
 package com.iucse.passnetbucket.domain.exception;
 
-public class FileNameExistedException extends RuntimeException{
+public class FileNameExistedException extends RuntimeException {
     public FileNameExistedException(String message) {
         super(message);
     }
