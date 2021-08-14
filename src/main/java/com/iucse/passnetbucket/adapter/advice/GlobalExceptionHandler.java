1d41d3b894d182981a484f50314f14d3ccf12ed4
DuongVu98 <duongvutung1998@gmail.com>
2021-08-15 02:06:02 +0700

    form violation

--- src/main/java/com/iucse/passnetbucket/adapter/advice/GlobalExceptionHandler.java
+++ src/main/java/com/iucse/passnetbucket/adapter/advice/GlobalExceptionHandler.java
@@ -8,12 +8,35 @@ import com.iucse.passnetbucket.domain.exception.FileNotExistException;
 import com.iucse.passnetbucket.domain.view.ErrorView;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler extends BaseController {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorView> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception
+           .getBindingResult()
+           .getAllErrors()
+           .forEach(
+              error -> {
+                  String fieldName = ((FieldError) error).getField();
+                  String errorMessage = error.getDefaultMessage();
+                  errors.put(fieldName, errorMessage);
+              }
+           );
+
+        return returnBadRequest(exception.getMessage(), errors);
+    }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(BucketNotFoundException.class)
